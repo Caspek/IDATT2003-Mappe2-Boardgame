2f614f805839cfdc7515b9e95792491628c16ed6
OskarHoddoLindh <oskar.lindh@hotmail.no>
2025-05-05 19:24:16 +0200

    satt inn første tekstkomponent + deaktivert fullscreen

--- src/main/java/edu/ntnu/iir/bidata/javafx/Javafx.java
+++ src/main/java/edu/ntnu/iir/bidata/javafx/Javafx.java
@@ -5,7 +5,10 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 // import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 
 public class Javafx extends Application {
@@ -21,14 +24,24 @@ public class Javafx extends Application {
         Group root = new Group();
         Scene scene = new Scene(root, Color.CORNFLOWERBLUE);
 
-        stage.setTitle("stigespillet");
+        stage.setTitle("L.A.D.D.E.R");
 
         stage.setWidth(800);
         stage.setHeight(800);
 
         stage.setResizable(false);
-        stage.setFullScreen(true);
+        // stage.setFullScreen(true); fjerner det mens jeg redigerer mye
 
+        Text section1 = new Text();
+        section1.setText("Section 1");
+        section1.setX(50);
+        section1.setY(50);
+        section1.setFont(Font.font("Verdana", 20));
+        section1.setFill(Color.GREEN);
+
+
+
+        root.getChildren().add(section1);
         stage.setScene(scene);
         stage.show();
 
