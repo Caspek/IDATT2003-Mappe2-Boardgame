b11942a2ac1a5b892727ff02fae952bbeadd6bd9
OskarHoddoLindh <oskar.lindh@hotmail.no>
2025-05-05 18:48:21 +0200

    OBS! har fjerna testklassene pga error som var i veien for produktivitet, merk dette før eventuell pull
    fikk til å sette opp javafx ved egen klasse til å kunne kjøre javaFX - kode for GUI legges foreløpig inn i Javafx-klassen, mens man launcher GUI fra Main klassen, som henter inn Javafx-klassen
    
    fikk hjelp fra dette innlegget
     (https://www.reddit.com/r/JavaFX/comments/k7aa9q/javafx_error_error_javafx_runtime_components_are/)
    lagt inn scene + farger

--- src/main/java/edu/ntnu/iir/bidata/Main.java
+++ src/main/java/edu/ntnu/iir/bidata/Main.java
@@ -1,5 +1,10 @@
 package edu.ntnu.iir.bidata;
+
+
+import edu.ntnu.iir.bidata.javafx.Javafx;
+
 public class Main {
-
-
+    public static void main(String[] args) {
+        Javafx.main(args);
+    }
 }
--- src/main/java/edu/ntnu/iir/bidata/javafx/Javafx.java
+++ src/main/java/edu/ntnu/iir/bidata/javafx/Javafx.java
@@ -1,4 +1,37 @@
 package edu.ntnu.iir.bidata.javafx;
+// package org.example;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+// import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class Javafx {
+
+public class Javafx extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.CORNFLOWERBLUE);
+
+        stage.setScene(scene);
+        stage.show();
+
+
+
+
+
+
+    }
+
+
 }
+
+
