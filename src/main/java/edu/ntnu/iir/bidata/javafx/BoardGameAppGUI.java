16647053e004e88b197073436c8598053bdc24ab
Caspek <ckolsrud1@gmail.com>
2025-05-22 22:47:25 +0200

    1.11 changes- - added a suppression for the name of the main class since checkstyle hates consecutive uppercase letters

--- src/main/java/edu/ntnu/iir/bidata/javafx/BoardGameAppGUI.java
+++ src/main/java/edu/ntnu/iir/bidata/javafx/BoardGameAppGUI.java
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
  * This class handles the setup screen, user input validation, and transitions to the game view.
  */
 
+@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 public class BoardGameAppGUI extends Application {
 
   /**
